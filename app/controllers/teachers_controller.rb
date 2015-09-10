f354721e9573a0615e82590af62be197e0a242b4
wulfman97 <calebschade97@gmail.com>
2015-09-10 16:36:30 -0600

    teachers controllers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,13 +1,43 @@
 class TeachersController < ApplicationController
   def index
+    @teachers = Teacher.all
   end
 
   def show
+    @teacher = Teacher.find(params[:id])
   end
 
   def new
+    @teacher = Teacher.new
+  end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      redirect_to teacher_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @teacher = Teacher.find(params[:id])
   end
+
+  def update
+    @teacher = Teacher.find(params[:id])
+    if @teacher.update(teacher_params)
+      redirect_to teachers_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name)
+  end
+
 end
+
