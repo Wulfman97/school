c05d08a8f7223036dd45c75cfe52be8c0ff03c28
wulfman97 <calebschade97@gmail.com>
2015-09-11 14:41:29 -0600

    assignments controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,13 +1,45 @@
 class AssignmentsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @course = Course.find(params[:course_id])
+    @assignment = @course.assignments.new
+  end
+
+  def create
+    @course = Course.find(params[:course_id])
+    @assignment = Assignment.new(assignment_params)
+    if @assignment.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end  
   end
 
   def edit
+    @course = Course.find(params[:id])
+    @assignment = Assignment.find(params[:course_id])
   end
+
+  def update
+    @course = Course.find(params[:course_id])
+    @assignment = Assignment.find(params[:id])
+    if @assignment.update(assignment_params)
+      redirect_to course_path(@course)
+    else 
+      render :edit 
+    end  
+  end
+
+  def destroy
+    @course = Course.find(params[:course_id])
+    @assignment = Assignment.find(params[:id])
+    @assignment.destroy
+      redirect_to course_path(@course)  
+  end  
+
+  private
+
+  def assignment_params
+      params.require(:assignment).permit (:title, :points_pos, :point_earned, :grade_earned)
+  end
+
 end
